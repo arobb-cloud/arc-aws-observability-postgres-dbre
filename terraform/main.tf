18c9f79e2339e4eb4860a361ab2c1142df3f129e
Anthony Robb <arobb.cloud@gmail.com>
2026-06-07 16:39:11 -0500

    Add CloudWatch billing alarm with SNS notification

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,3 +10,24 @@ resource "aws_sns_topic_subscription" "email_alerts" {
   protocol  = "email"
   endpoint  = var.alert_email
 }
+
+resource "aws_cloudwatch_metric_alarm" "billing_alarm_test" {
+  alarm_name          = "${var.project_name}-${var.environment}-estimated-charges-test"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "EstimatedCharges"
+  namespace           = "AWS/Billing"
+  period              = 21600
+  statistic           = "Maximum"
+  threshold           = 1
+  alarm_description   = "Test CloudWatch alarm for estimated AWS charges greater than 1 USD"
+  treat_missing_data  = "notBreaching"
+
+  dimensions = {
+    Currency = "USD"
+  }
+
+  alarm_actions = [
+    aws_sns_topic.alerts.arn
+  ]
+}
